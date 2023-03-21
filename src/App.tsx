f284a057db2838c892815e64159dd75683650d72
harryg15 <harry.grant99@googlemail.com>
2023-03-21 17:33:58 +0000

    added tailwind and daisyui for easier css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Home } from "./pages"
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from "framer-motion"
+import './index.css'
 
 const App = (): JSX.Element => {
+  const location = useLocation()
+
   return (
-    <div>
-      <h1>I am in the App.tsx</h1>
-      <Home />
+    <div className="App">
+      
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AnimatePresence>
+
     </div>
   )
 }
