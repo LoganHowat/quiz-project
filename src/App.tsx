5a00b777e9a579904ed593367c238535556a67aa
harryg15 <harry.grant99@googlemail.com>
2023-03-21 18:14:07 +0000

    made intial home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Home } from "./pages"
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from "framer-motion"
-import './index.css'
+import SquaresBG from "react-animated-squares"
+import "./App.css"
 
 const App = (): JSX.Element => {
   const location = useLocation()
 
   return (
     <div className="App">
+      <SquaresBG count={40} speed={0.7} backgroundColor={"linear-gradient(-45deg, #ee7752, #e73c7e)"}/>
       
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
-import { Navbar, Login, Signup } from '../../components'
+import { Login, Signup } from '../../components'
+import { motion } from "framer-motion"
 
 const Home = (): JSX.Element => {
   return (
-    <div>
-        <h1>This is the home page</h1>
-        <Navbar />
-        <Login />
-        <Signup />
-    </div>
+        <div className="title-container">
+          <motion.h1
+            className="title"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 0.9 }}
+            exit={{ opacity: 0, scale: 0.5 }}
+            whileHover={{ scale: 0.8 }}
+            whileTap={{ scale: 0.75 }}
+            transition={{
+              default: {
+                duration: 0.3,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+              scale: {
+                type: "spring",
+                damping: 10,
+                stiffness: 400,
+                restDelta: 0.001,
+              },
+            }}
+          >
+            WELCOME!
+          </motion.h1>
+        </div>
   )
 }
 
